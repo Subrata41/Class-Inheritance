1818db7fb63d8be8eb62015b6bb7a920832e6bfe
Subrata Roy <113381826+Subrata41@users.noreply.github.com>
2023-01-25 23:28:14 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
 
+    drawShape() {
+        console.log(`Drawing a shape with color: ${this.color}`);
+    }
+
+    calculateArea() {
+        console.log("Calculating area of shape");
+    }
+}
 // export class using module.exports
+module.exports = Shape;
